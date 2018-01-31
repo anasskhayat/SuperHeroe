3b33fffc3b5a92a6c626d88702d12847d6f3dccc
anasskhayat <figo1899@gmail.com>
2018-01-31 18:29:41 +0100

    ejemplo sencillo

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HeroesService, heroe } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+
+private heroes:heroe[]= [];
+
+  constructor( private _heroesService: HeroesService,
+              private router:Router) 
+  {
+    console.log("constructor");
+   }
+
+  ngOnInit() {
+
+    this.heroes=this._heroesService.getHeroes();
+    console.log(this.heroes);
+  }
+
+  verHeroe(idx:number){
+   this.router.navigate( ['/heroe',idx] );
+  }
+
+}
